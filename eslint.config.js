c124b11d24e6e070122dfab975e5d144efed36f7
Thunder <aakarshkashyap143341@gmail.com>
2025-06-30 21:57:00 +0530

    Add linting config files for HTML, CSS, JS

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    ignores: ["node_modules"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
